1c5d7f428b3aef9a7ade2934649d56c58f987a77
Alexander Chori <alexandrchori@gmail.com>
2020-12-31 16:17:07 +0300

    Add selector for architecture

--- gui/src/main/kotlin/ru/itmo/chori/archcomparator/gui/Main.kt
+++ gui/src/main/kotlin/ru/itmo/chori/archcomparator/gui/Main.kt
@@ -1,15 +1,72 @@
 package ru.itmo.chori.archcomparator.gui
 
+import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.FXCollections
+import javafx.stage.Stage
 import tornadofx.*
 
-class MainView : View("Test view") {
+const val WINDOW_WIDTH = 500.0
+
+class Architecture(val name: String, val description: String) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+class MainView : View("Architecture Comparator") {
+    private val architectures = FXCollections.observableArrayList(
+        Architecture(
+            "2 threads for each client",
+            "Server creates a separate thread for receiving queries and a separate thread for sending " +
+                    "responses for each client"
+        ),
+        Architecture(
+            "Non-blocking",
+            "Server has one thread with selector for receiving queries in non-blocking manner and one " +
+                    "thread with selector for sending responses"
+        ),
+        Architecture(
+            "Asynchronous",
+            "All reads and writes on server are implemented in asynchronous manner: it has some thread " +
+                    "pool, where id does all reads and writes. And after each (non-)successful operation, " +
+                    "corresponding callback is called"
+        )
+    )
+    private val selectedArchitecture = SimpleObjectProperty(architectures[0])
+    private val selectedArchitectureDescription = selectedArchitecture.stringBinding { it?.description }
+
     override val root = borderpane {
+        top {
+            label("ArchComparator")
+        }
+
         center {
-            label("Hello, world!")
+            form {
+                fieldset("Server architecture") {
+                    field("Choose server architecture") {
+                        combobox(selectedArchitecture, architectures)
+                    }
+
+                    text(selectedArchitectureDescription) {
+                        style {
+                            wrappingWidth = WINDOW_WIDTH - 20.0
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-class ArchitectureComparatorApp : App(MainView::class)
+class ArchitectureComparatorApp : App(MainView::class) {
+    override fun start(stage: Stage) {
+        with(stage) {
+            maxWidth = WINDOW_WIDTH
+            isResizable = false
+
+            super.start(this)
+        }
+    }
+}
 
 fun main(args: Array<String>): Unit = launch<ArchitectureComparatorApp>(args)
