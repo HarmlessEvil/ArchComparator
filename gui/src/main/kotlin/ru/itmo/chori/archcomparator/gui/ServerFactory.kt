f0e1503be9076ad72f34bf95d8285e32604c0fe0
Alexander Chori <alexandrchori@gmail.com>
2021-01-06 00:33:53 +0300

    Add charts

--- gui/src/main/kotlin/ru/itmo/chori/archcomparator/gui/ServerFactory.kt
+++ gui/src/main/kotlin/ru/itmo/chori/archcomparator/gui/ServerFactory.kt
@@ -1,6 +1,9 @@
 package ru.itmo.chori.archcomparator.gui
 
-import ru.itmo.chori.archcomparator.server.*
+import ru.itmo.chori.archcomparator.server.AsynchronousServer
+import ru.itmo.chori.archcomparator.server.ManyThreadedServer
+import ru.itmo.chori.archcomparator.server.NonBlockingServer
+import ru.itmo.chori.archcomparator.server.ServerWithStatistics
 
 fun serverFactory(architecture: Architecture, port: Int, threadPoolSize: Int): ServerWithStatistics {
     return when(architecture) {
